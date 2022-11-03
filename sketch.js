2bd704ac1d747fe3e6f0290b3489b64d8b7c30b5
Pimaia <97753929+Pimaia@users.noreply.github.com>
2022-11-03 13:48:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var fazenda;
 var up, up1, up2, up3;
 var detetive, predio, azul, blue, it;
+var vida, morte;
 
 
 function preload(){
@@ -18,9 +19,10 @@ function setup() {
 
   up = createSprite(90, 150, 50, 50);
   up.addAnimation("altasaventuras", up1);
-  up.scale = 0.5;
+  up.scale = 0.45;
 
-
+  vida = new Group();
+  morte = new Group();
 }
 
 function draw() {
@@ -31,5 +33,54 @@ function draw() {
   }
   up.velocityY += 1.3;
 
+  ar();
+  terra();
+
   drawSprites();
+}
+
+function ar (){
+  if(frameCount % 80 === 0){
+    oxigenio = createSprite(1600, random(0, height/2), 45, 45);
+    oxigenio.velocityX = -5;
+    pi = Math.round(random(1,2));
+    switch(pi){
+      case 1: oxigenio.addImage(blue)
+              oxigenio.scale = 0.15;
+      break;
+      case 2: oxigenio.addImage(it)
+              oxigenio.scale = 0.15;
+      break;
+      default: break;
+    }
+    oxigenio.lifetime = 330;  
+
+    vida.add(oxigenio);
+  }
+}
+
+function terra (){
+  if(frameCount % Math.round (random (60, 100)) === 0){
+    solo = createSprite(1600, 750, 45, 45);
+    solo.velocityX = -5;
+    circulo = Math.round(random(1, 3));
+    switch(circulo){
+      case 1: solo.addImage(detetive)
+              solo.scale = 0.25;
+              solo.y = 590;
+      break;
+      case 2: solo.addImage(predio)
+              solo.scale = 0.25;
+              solo.y = 570;
+      break;
+      case 3: solo.addImage(azul)
+              solo.scale = 0.15;
+              solo.y = 680;
+      break;
+      default: break;
+    }
+    solo.lifetime = 370;  
+
+    morte.add(solo);
+  }
 }
